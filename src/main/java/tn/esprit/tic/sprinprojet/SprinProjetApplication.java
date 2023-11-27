f485c95649939d3d291428650a37c49f2d879857
Fatenksouri <faten.ksouri@esprit.tn>
2023-11-27 00:30:33 +0000

    Scheduler

--- src/main/java/tn/esprit/tic/sprinprojet/SprinProjetApplication.java
+++ src/main/java/tn/esprit/tic/sprinprojet/SprinProjetApplication.java
@@ -2,8 +2,9 @@ package tn.esprit.tic.sprinprojet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableScheduling
 public class SprinProjetApplication {
 
     public static void main(String[] args) {
